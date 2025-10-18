5e099d25806046589a0d67c2ab1fd96b6256e02c
KendellTaylor42 <kendelltaylor0@gmail.com>
2025-10-17 20:45:08 -0500

    semi functionally exploration code and fix launch file1

--- src/retail_assistant_bringup/launch/gazebo_sim_slam.launch.py
+++ src/retail_assistant_bringup/launch/gazebo_sim_slam.launch.py
@@ -29,7 +29,6 @@ def generate_launch_description():
                     launch_arguments={'world': os.path.join(get_package_share_directory(package_name),'worlds','fake_store_layout.world')}.items()#os.path.join(get_package_share_directory(package_name),'worlds','fake_store_layout')
              )
     print(os.path.join(get_package_share_directory(package_name),'worlds','fake_store_layout.world'))
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'retail_assistant'],
