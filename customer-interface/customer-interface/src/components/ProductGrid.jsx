f044d76f6c1301f7ea268df1d806ad46f51da05d
AsianPantsMan <casualcaseytime@gmail.com>
2025-10-21 11:38:20 -0500

    change card format

--- customer-interface/customer-interface/src/components/ProductGrid.jsx
+++ customer-interface/customer-interface/src/components/ProductGrid.jsx
@@ -1,12 +1,12 @@
 export default function ProductGrid({ products }) {
     if (!products.length) return <div>No items found</div>;
     return (
-        <div className="grid grid-cols-2 gap-3">
+        <div className="grid grid-cols-3 gap-4">
             {products.map(p => (
-                <div key={p.productName} className="bg-white rounded shadow p-2">
+                <div key={p.productName} className="bg-white rounded shadow p-2 min-h-[220px] min-w-[280px]">
                     <img src={p.image} alt={p.productName} className="w-full h-32 object-cover rounded" />
-                    <div className="mt-2 text-sm font-medium">{p.productName}</div>
-                    <div className="text-xs text-gray-600">{p.productCategory} — ${p.price}</div>
+                    <div className="mt-2 text-m text-gray-600 font-medium">{p.productName}</div>
+                    <div className="text-s text-gray-600">{p.productCategory} — ${p.price}</div>
                 </div>
             ))}
         </div>
