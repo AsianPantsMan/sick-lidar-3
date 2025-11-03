80da721ed32ddb6fdbcc0bbcc152b4c15f1671c9
KendellTaylor42 <kendelltaylor0@gmail.com>
2025-11-03 17:58:07 -0600

    Reverted back to Gazebo Classic setup

--- src/retail_assistant_bringup/launch/gazebo_sim.launch.py
+++ src/retail_assistant_bringup/launch/gazebo_sim.launch.py
@@ -4,9 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
 
 
@@ -25,47 +25,24 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the ros_ign_gazebo package
-
-    default_world = os.path.join(
-        get_package_share_directory(package_name),
-        'worlds',
-        'empty.world')
-    
-    world= LaunchConfiguration('world')
-
-    world_arg=DeclareLaunchArgument('world',
-                              default_value=default_world,
-                              description='SDF world file to load')
-    
+    # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_ign_gazebo'),'launch','ign_gazebo.launch.py')]),
-                    launch_arguments={'ign_args':['-r ' , world], 'on_exit_shutdown': 'true'}.items()            #os.path.join(get_package_share_directory(package_name),'worlds','fake_store_layout')
+                    get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py')]),
+                    launch_arguments={'world': '/home/kendell/retail_assistant_ws/src/retail_assistant_bringup/worlds/fake_store_layout.world'}.items()#os.path.join(get_package_share_directory(package_name),'worlds','fake_store_layout')
              )
 
-    # Run the spawner node from the ros_ign_gazebo package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_ign_gazebo', executable='create',
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-name', 'retail_assistant',
-                                   '-z', '0.1'],
+                                   '-entity', 'retail_assistant'],
                         output='screen')
 
-    bridge_params = os.path.join(get_package_share_directory(package_name),'config','ignition_bridge.yaml')
-    ign_bridge = Node(
-        package='ros_ign_bridge',
-        executable='parameter_bridge',
-        parameters=[
-            '--ros-args',
-            '-p',
-            f'config_file:={bridge_params}',
-        ]
-    )
+
+
     # Launch them all!
     return LaunchDescription([
         rsp,
-        world_arg,
         gazebo,
         spawn_entity,
-        ign_bridge,
     ])
